7388fe1d3480095bfa275043f4a226022624334e
Tyrael <xykingofskillyx@gmail.com>
2023-06-15 21:05:00 +0200

    Initial commit

---
+++ src/main/java/de/ethosteam/EthosTeam.java
@@ -0,0 +1,17 @@
+package de.ethosteam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EthosTeam extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
